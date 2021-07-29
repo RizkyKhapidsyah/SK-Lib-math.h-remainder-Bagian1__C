9f50018a6c53c75ddcf73c1e2d8ff252e53ade42
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-29 20:59:41 +0700

    Add project files.

---
+++ SK-Lib-math.h-remainder-Bagian1__C/Source.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+/*
+	Source by CPlusPlus
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+int main() {
+	printf("remainder of 6.4 / 2 is %f\n", remainder(6.4, 2));
+	printf("remainder of 89.3 / 7.3 is %f\n", remainder(89.3, 7.3));
+	_getch();
+	return 0;
+}
